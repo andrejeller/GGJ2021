c6258dfacea382454cb2becc0764c96dea0ada04
andrejeller <jeller.andre@gmail.com>
2021-01-30 13:52:46 -0300

    Primeiros testes mic

--- GGJ2021/Assets/Scripts/MicrofoneVaiDarBoa.cs
+++ GGJ2021/Assets/Scripts/MicrofoneVaiDarBoa.cs
@@ -17,7 +17,7 @@ public class MicrofoneVaiDarBoa : MonoBehaviour {
 
         audioSource.Stop();
 
-        audioSource.clip = Microphone.Start(padrao, true, 10, AudioSettings.outputSampleRate);
+        audioSource.clip = Microphone.Start(padrao, false, 10, AudioSettings.outputSampleRate);
         audioSource.loop = true;
         
         
@@ -45,11 +45,22 @@ public class MicrofoneVaiDarBoa : MonoBehaviour {
 
         AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
 
+        /*
         for (int i = 1; i < spectrum.Length - 1; i++) {
             Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
             Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
             Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
             Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+        }*/
+
+
+        for (int i = 0; i < 10; i++) {
+            if (spectrum[i] * 100000 > 60) {
+                Debug.Log("OPA");
+            }
         }
+
+        
+
     }
 }
